b335466ba10750c13a5fe6d955e57692149221e3
angeljs-exe <s21910263@student.unklab.ac.id>
2022-02-15 22:13:41 +0800

    Nama Anggota Kelompok

---
+++ index.js
@@ -0,0 +1,8 @@
+/* Nama Anggota Kelompok:
+1. Desayli, Andrey
+2. Angelita Jeanette
+3. Kollabathula, Suvin Raj
+4. Liando, Itou Billy
+5. Rampengan, Elshadai Gracia Carolina
+6. Tewu, Venisa Alsye Octriane
+*/
